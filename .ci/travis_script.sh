6eef2f5164af401f10b9d79fe9509f0c2ffda83a
rahulgit-ps <62048005+rahulgit-ps@users.noreply.github.com>
2020-08-14 11:49:13 +0530

    Update travis_script.sh

--- .ci/travis_script.sh
+++ .ci/travis_script.sh
@@ -10,7 +10,7 @@ set -e -x
 if [[ -n ${TOXENV} ]]; then
   tox -e ${TOXENV}
 elif [[ ${BIGCHAINDB_CI_ABCI} == 'enable' ]]; then
-  docker-compose exec bigchaindb pytest -v -m abci
+  docker-compose exec -T bigchaindb pytest -v -m abci
 elif [[ ${BIGCHAINDB_ACCEPTANCE_TEST} == 'enable' ]]; then
     ./run-acceptance-test.sh
 else
